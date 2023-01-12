91a046278992222d267b4737c109f5cb312d2bfd
CJW-MAPU <c.j.biz.woong@gmail.com>
2023-01-12 16:50:21 +0900

    refactor: ground setting about can-fd raw data processing

---
+++ exception/ExceptionController.py
@@ -0,0 +1,3 @@
+def CallTypeMatchException():
+    raise Exception('Do not match between dataset type and attack type.\n'
+                    'If need you help, please read help.')
---
+++ exception/TypeMatchException.py
@@ -0,0 +1,3 @@
+class TypeMatchException(Exception):
+    def __init__(self):
+        super.__init__()
